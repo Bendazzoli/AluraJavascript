8eb907579fdfc748cc27640d37b265fe4f91ff57
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-24 16:43:14 -0300

    Incluindo form de cadastro na lista

--- AparecidaNutricao/js/principal.js
+++ AparecidaNutricao/js/principal.js
@@ -34,3 +34,35 @@ for(var i=0; i < pacientes.length; i++){
         paciente.classList.add("paciente-invalido");
     }
 }
+
+var botaoAddPaciente = document.querySelector("#adicionar-paciente");
+
+botaoAddPaciente.addEventListener("click", function(event) {
+    event.preventDefault();
+    var formAdicionaPaciente = document.querySelector("#formAdicionaPaciente");
+    var peso = formAdicionaPaciente.peso.value;
+    var altura = formAdicionaPaciente.altura.value;
+    var gordura = formAdicionaPaciente.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = formAdicionaPaciente.nome.value;
+    pesoTd.textContent = formAdicionaPaciente.peso.value;
+    alturaTd.textContent = formAdicionaPaciente.altura.value;
+    gorduraTd.textContent = formAdicionaPaciente.gordura.value;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabelaPacientes = document.querySelector("#tabela-pacientes");
+    tabelaPacientes.appendChild(pacienteTr);
+    
+});
