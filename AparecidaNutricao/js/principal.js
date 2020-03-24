9ca1891ad28a7a414c8ec46f0883984626bd1b8c
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-24 12:41:13 -0300

    calculando o IMC de um paciente

--- AparecidaNutricao/js/principal.js
+++ AparecidaNutricao/js/principal.js
@@ -1,2 +1,12 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro_paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+tdImc.textContent = peso / (altura*altura);
