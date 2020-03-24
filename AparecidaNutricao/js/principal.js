4a138e081798fd474b76b462f2c51e23add27743
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-24 13:33:41 -0300

    Calculando o IMC para todos os pacientes

--- AparecidaNutricao/js/principal.js
+++ AparecidaNutricao/js/principal.js
@@ -1,26 +1,34 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro_paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var imcPossivel = true;
 
-if(peso <= 0 || peso >= 1000){
-    imcPossivel = false;
-    tdImc.textContent = "Peso inválido";
-}
+for(var i=0; i < pacientes.length; i++){
 
-if(altura <= 0 || altura >= 3){
-    imcPossivel = false;
-    tdImc.textContent = "Altura inválido";
-}
+    var paciente = pacientes[i];
 
-if(imcPossivel){
-    tdImc.textContent = peso / (altura*altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var imcPossivel = true;
+
+    if(peso <= 0 || peso >= 1000){
+        imcPossivel = false;
+        tdImc.textContent = "Peso inválido";
+    }
+
+    if(altura <= 0 || altura >= 3){
+        imcPossivel = false;
+        tdImc.textContent = "Altura inválido";
+    }
+
+    if(imcPossivel){
+        tdImc.textContent = (peso / (altura*altura)).toFixed(2);
+    }
 }
