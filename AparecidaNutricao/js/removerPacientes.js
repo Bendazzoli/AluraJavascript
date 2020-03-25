a8a47f7874a027cc4259b08666e125f1bcdf6751
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-25 14:49:34 -0300

    melhorando a experiencia do usuario no evento de deletar uma linha da tabela

--- AparecidaNutricao/js/removerPacientes.js
+++ AparecidaNutricao/js/removerPacientes.js
@@ -6,7 +6,11 @@ tabela.addEventListener("dblclick", function(event){
     //event.target é quem foi clicado 2x, no caso um TD
     //.parentNode é o pai do evento clicado, no caso o TR (que é a linha toda)
     //sendo assim, remove a linha toda, como desejamos 
-    event.target.parentNode.remove();
+    
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove(); 
+    }, 700);
 });
 
 //pacientes.forEach(function(paciente){
