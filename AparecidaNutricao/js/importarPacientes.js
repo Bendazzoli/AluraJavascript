e0817469486ab836d89b4fc6cfafb64daceba23d
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-25 18:09:00 -0300

    Tratando a requisição GET da API do servidor

--- AparecidaNutricao/js/importarPacientes.js
+++ AparecidaNutricao/js/importarPacientes.js
@@ -1,16 +1,25 @@
 var botaoImportar = document.querySelector("#importar-paciente");
 botaoImportar.addEventListener("click", function(){
     
-    //importar paciente-> http://api-pacientes.herokuapp.com/pacientes
+    var erroImportacao = document.querySelector("#erroImportacao");
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function(){
-        var pacientes = JSON.parse(xhr.responseText);
+
+        if(xhr.status == 200){
+            var pacientes = JSON.parse(xhr.responseText);
 
         pacientes.forEach(function(paciente){
             adicionaPacienteNaTabela(paciente);
         });
+        }else{
+            erroImportacao.innerHTML = "";
+            var li = document.createElement("li");
+            li.textContent = "Problema ao importar pacientes. Erro: " + xhr.responseText;
+            erroImportacao.appendChild(li);
+            erroImportacao.classList.add("mensagem-erro");
+        } 
     });
 
     xhr.send();
