bac9c9281743ec75ab7e5a9c1693b27496360954
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-03-25 16:42:17 -0300

    Criando mecanismo de pequisa na tela

---
+++ AparecidaNutricao/js/buscar.js
@@ -0,0 +1,24 @@
+var filtrarTabela = document.querySelector("#filtrar-tabela");
+
+filtrarTabela.addEventListener("input", function(){
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            
+            if(nome != this.value){
+                paciente.classList.add("fadeOutLinha");
+            }else{
+                paciente.classList.remove("fadeOutLinha");
+            }
+        }
+    }else{
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("fadeOutLinha");
+        }
+    }
+});
