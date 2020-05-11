b5a4819375c4baf8f09dc4a0be795660b80761e9
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-05-11 16:07:19 -0300

    Requisições Ajax com o objeto XMLHttpRequest

--- Negociacoes/client/js/app/controllers/NegociacaoController.js
+++ Negociacoes/client/js/app/controllers/NegociacaoController.js
@@ -31,6 +31,23 @@ class NegociacaoController {
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
+
+    importaNegociacoes(){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:3000/negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4 ){
+                if(xhr.status == 200){
+                    console.log('Obtendo as negociações do servidor.');
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.');
+                }
+            }
+        };
+
+        xhr.send();
+    }
     
     _criaNegociacao() {
         
